e2ff5a58023378c5a1cb34a7b3248cb4ab24d590
kenoll <kelseynoll@gmail.com>
2017-03-15 14:30:43 -0400

    reorganization and clean-up

--- locus_haplotype.R
+++ locus_haplotype.R
@@ -1,11 +1,6 @@
+library(dplyr)
+
 #### to give haplotypes ####
-
-##notes: certain user inputs are required and are described here
-#line 7: set working directory/indicate where you have saved the master founder mosaic file
-#line 11: chromosome number, start, and end location information for gene/locus/region of interest
-#line 29: (optional) - see intructions for sorting out founders into categories (e.g. mx1 status or positive staining for a marker)
-#line 55: indication of where you want to name/output the file to be generated
-
 
 #call up master founder mosaic file
 setwd("~/Dropbox/Heise/CC/cc_founder_mosaics")
@@ -14,12 +9,11 @@ mosaics=read.csv("founder_mosaics.csv")
 #selection region of interest based on chromosome number, start position, and end position
 #will not include strain if the region is not in a 'founder block' i.e. spans multiple rows of the data frame
 chrom=11
-# start=66376670
-# end=66391310
 
-start=70500000
-end=72000000
+start=69000000
+end=72500000
 
+#### set regions ####
 region.1=subset(mosaics,
                 mosaics$start_position<=start & 
                 mosaics$end_position>=end & 
@@ -45,43 +39,20 @@ region=merge(region.2,region.3,all=T)
 region=merge(region,region.4,all=T)
 # region=merge(region.1,region.merge,all=T) #regions 2 and 3 already contain all in region 1
 
-#region.diff highlights lines with a haplotype change within the region
-library(dplyr)
-# region.diff=setdiff(region,region.1)
-region.diff=anti_join(region,region.1)
-#will eliminate a non-changing second haplotype
+#### haplotype changes
 
 #eliminate second haplotype if strain has same founder for both (most cases)
+#if there's a double recombination so that the same founders repeat, it will only keep one of each
 region$alias_founder=paste(region$alias,region$founder,sep="_")
-# region = region[order(region$haplotype),] 
-# region = region[order(region$strain),]
 region = region[!duplicated(region$alias_founder),]
 region = region[1:7]
-#if there's a double recombination so that the same founders repeat, it will only keep one of each
 
-region.diff$alias_founder=paste(region.diff$alias,region.diff$founder,sep="_")
-region.diff = region.diff[!duplicated(region.diff$alias_founder),]
-region.diff = region.diff[1:7]
-
-## optional - select status of region based on founder by uncommenting applicable code (two or three ways)
+#select status of region based on founder by uncommenting applicable code (two or three ways)
 
 # for two cases only (e.g. positive for staining), change/add/delete letters for founder strains to be marked as 'true'
 region$status=
   ifelse(region$founder=="H",
          "0","1")
-
-region.diff$status=
-  ifelse(region.diff$founder=="H",
-         "0","1")
-
-# delete duplicates for lines where the founder haplotype change doesn't change the allele status
-region.diff$alias_status=paste(region.diff$alias,region.diff$status,sep="_")
-# region.diff = region.diff[!duplicated(region.diff$alias_status),]
-# region.diff = region.diff[1:8]
-
-changes=which(duplicated(region.diff["alias_status"])|(duplicated(region.diff["alias_status"],fromLast=T)))
-changes=region.diff[changes,]
-changes=setdiff(region.diff,changes)
 
 # ## for a three way case (e.g. mx1 status)
 # group1="dom"
@@ -100,19 +71,41 @@ changes=setdiff(region.diff,changes)
 #           ,group2,group3)
 #   )
 
+
 #output csv
 #user input where you want to save the file
-file="~/Dropbox/Heise/ELISA Antibody/qtls/d10/chr5_62-75_status.csv"
-write.csv(region,file,row.names=F)
+filepath="~/Dropbox/Heise/ELISA Antibody/qtls/d10/"
+filename=paste(chrom,"_",start/1000000,"-",end/1000000,"_","status",sep="")
 
-file="~/Dropbox/Heise/ELISA Antibody/qtls/d10/chr5_transitionsonly_status.csv"
-write.csv(region.diff,file,row.names=F)
-
-file="~/Dropbox/Heise/ELISA Antibody/qtls/d10/chr5_meaningful_transitionsonly_status.csv"
-write.csv(changes,file,row.names=F)
+write.csv(region,paste0(filepath,filename,".csv"),row.names=F)
 
 
 
+#### highlight lines with haplotypes changes within the region of interest
+
+#region.diff highlights lines with a haplotype change within the region
+#will eliminate a non-changing second haplotype
+region.diff=anti_join(region,region.1)
+region.diff$alias_founder=paste(region.diff$alias,region.diff$founder,sep="_")
+region.diff = region.diff[!duplicated(region.diff$alias_founder),]
+region.diff = region.diff[1:7]
+
+#set allele status based on founder haplotype
+region.diff$status=
+  ifelse(region.diff$founder=="H",
+         "0","1")
+
+# delete duplicates for lines where the founder haplotype change doesn't change the allele status
+region.diff$alias_status=paste(region.diff$alias,region.diff$status,sep="_")
+changes=which(duplicated(region.diff["alias_status"])|(duplicated(region.diff["alias_status"],fromLast=T)))
+changes=region.diff[changes,]
+changes=setdiff(region.diff,changes)
+
+#export .csv
+filepath="~/Dropbox/Heise/ELISA Antibody/qtls/d10/"
+filename=paste(chrom,"_",start/1000000,"-",end/1000000,"_","status_recombinations",sep="")
+
+write.csv(changes,paste0(filepath,filename,".csv"),row.names=F)
 
 
 
