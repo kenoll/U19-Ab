d78254fc98db86c1fd5ec5767081c6eb8d54bbd0
kenoll <kelseynoll@gmail.com>
2017-03-24 14:59:02 -0400

    updating code to work with new file organization scheme, only doing perms from loaded qtls

--- qtl_mapping.R
+++ qtl_mapping.R
@@ -13,10 +13,13 @@ dat.10=read.csv(paste("~/Dropbox/Heise/U19-Ab/data_bin/d10/dat.10.",data.type,".
 dat.15=read.csv(paste("~/Dropbox/Heise/U19-Ab/data_bin/d15/dat.15.",data.type,".csv",sep=""))
 dat.45=read.csv(paste("~/Dropbox/Heise/U19-Ab/data_bin/d45/dat.45.",data.type,".csv",sep=""))
 
-#set list of anitbody isotypes in the column order they appear in
-abs=c("IgG1" ,  "IgG2ac" , "IgG2b" , "IgG3" ,  "IgM"  ,  "TotalG")
+#set number of header columns before data starts
+hc=7
 
-col.list=colnames(wideauc)
+#set list of antibody isotypes in the column order they appear in
+abs=c("IgG1" ,  "IgG2ac" , "IgG2b" , "IgG3" ,  "IgM"  ,  "TotalG")
+col.list=colnames(dat.10)
+
 
 #### lists ####
 day.list=list(7,10,15,45)
@@ -38,46 +41,9 @@ for(k in 1:4)
     g+geom_point(aes(colour=assay_date))+theme_classic()+
       labs(title=paste("Day",day.list[[k]],abs[i],sep=" "),y="RIX",x="AUC")
     
-    # ggsave(file.path(paste("phenotypes/d",day,sep=""),paste("b6_pheno_dist_batch_",abs[i],"_d",day.list[[k]],".jpg",sep="")), width=6, height=9)
+    ggsave(file.path(paste("phenotypes/d",day,"/pheno_dist",sep=""),paste("pheno_dist_",abs[i],"_",data.type,"_d",day.list[[k]],".jpg",sep="")), width=6, height=9)
   }
 }
-
-
-#geom_point(aes(colour=assay_date))
-#+ geom_point(colour="purple")
-
-# # test individually # #
-# dat=dat.10
-# i=1
-# 
-# phenotype=dat[c(1:4,i+4)]
-# phenotype=phenotype[complete.cases(phenotype),]  
-# 
-# g<-ggplot(phenotype, aes_string(x=abs[i],y=paste0("reorder(RIX,",abs[i],")")))
-# g+geom_point(aes(color=assay_date))+theme_classic()+
-#   labs(title=paste("Day",day.list[[k]],abs[i],sep=" "),y="RIX",x="AUC")
-#   # +  geom_text(aes(label=ID))
-
-### phenotypic distribution plots / old code = loops for all isotypes of one day at a time ###
-# 
-# day=7
-# # (paste0("dat.",day))
-# dat=dat.7
-# 
-# for(i in 1:6)
-# {
-#   phenotype=dat[c(1:4,i+4)]
-#   dat=dat[complete.cases(dat),]  
-# 
-# g<-ggplot(dat, aes_string(x=abs[i],y=paste0("reorder(RIX,",abs[i],")")))
-# g+geom_point(aes(color=assay_date))+theme_classic()+
-#   labs(title=paste("D10",abs[i],sep=" "),y="RIX",x="AUC")
-# 
-# ggsave(file.path(paste("qtls/d",day,sep=""),paste("pheno_dist_",abs[i],".jpg",sep="")), width=6, height=9)
-# }
-# 
-# # "RIX"    "ID"     "day"    "virus"  "IgG1"   "IgG2ac" "IgG2b"  "IgG3"   "IgM"    "TotalG"
-
 
 #### heritability calculations ####
 heritability.bounds=NULL
@@ -92,7 +58,7 @@ for(k in 1:4)
   
   for(i in 1:6)
   {
-    a<-anova(lm(dat[,i+4]~dat$RIX))
+    a<-anova(lm(dat[,i+hc]~dat$RIX))
     interclass.corr<-(a$"Mean Sq"[1] - a$"Mean Sq"[2])/(a$"Mean Sq"[1]+(N-1)*a$"Mean Sq"[2])
     Coef.Genet.Det<-(a$"Mean Sq"[1] - a$"Mean Sq"[2])/(a$"Mean Sq"[1]+((2*N)-1)*a$"Mean Sq"[2])
     h.b=c(interclass.corr,Coef.Genet.Det)
@@ -105,7 +71,7 @@ for(k in 1:4)
   colnames(heritability.bounds)=c("upper","lower","day")
   
   write.csv(heritability.bounds,
-            file.path(paste("qtls/d",day,sep=""),paste("heritability_estimates_d",day.list[[k]],"_",run.date,".csv",sep="")),
+            file.path(paste("phenotypes/d",day,sep=""),paste("heritability_estimates_",data.type,"_d",day.list[[k]],".csv",sep=""))
   )
   heritability.bounds=NULL
 }
@@ -119,92 +85,92 @@ for(k in 1:4)
   V<-var(dat[,5:10], na.rm=T)
   C<-cov2cor(V)
   
-  jpeg(file.path(paste("qtls/d",day,sep=""),paste("correlation_matrix_d",day.list[[k]],"_",run.date,".jpg",sep="")))
+  jpeg(file.path(paste("phenotypes/d",day,sep=""),paste("correlation_matrix_",data.type,"_d",day.list[[k]],".jpg",sep="")))
   heatmap(C, symm=TRUE, col = cm.colors(256))
   dev.off()
 }
 
-#### two-phenotype distributions ####
-library(doBy)
-dat=dat.10
-
-Map.dat=summaryBy(IgG1+IgG2ac+IgG2b+IgG3+IgM+TotalG ~ 
-                    RIX + day, data=dat, FUN=mean, na.rm=T)
-colnames(Map.dat)[3:8] = gsub(".mean","",colnames(Map.dat[3:8]))
-Map.dat$RIX=as.character(Map.dat$RIX)
-
-#code with Mx1 status
-
-RIX_sep <- data.frame(do.call("rbind", strsplit(Map.dat$RIX,"x")))
-colnames(RIX_sep)[1:2]=c("dam","sire")
-
-Map.dat=cbind(RIX_sep,Map.dat)
-
-mx1=read.csv("~/Dropbox/Heise/CC/mx1_status.csv")
-mx1=mx1[c(1,8)]
-
-colnames(mx1)[1]="dam"
-Map.dat=merge(Map.dat,mx1)
-colnames(Map.dat)[11]="dam.mx1"
-
-colnames(mx1)[1]="sire"
-Map.dat=merge(Map.dat,mx1)
-colnames(Map.dat)[12]="sire.mx1"
-
-Map.dat=Map.dat[c(1,12,2,11,3:10)]
-
-#graphs
-Map.dat$RIX=as.factor(Map.dat$RIX)
-
-phenotype=Map.dat[c(1:6,12,11)]
-phenotype=phenotype[complete.cases(phenotype),]  
-
-cbPalette <- c("royalblue","orchid","seagreen2")
-
-g=ggplot(phenotype,aes(x=TotalG,y=IgM))
-g+geom_point(size=4,aes(color=dam.mx1))+
-  scale_color_manual(values=cbPalette)+
-  geom_point(aes(color=sire.mx1))+
-  theme_classic()+geom_text(aes(label=RIX),size=3,vjust=-1)+
-  theme(legend.title=element_blank())
-
-
-# rix.list=c("CC037xCC046","CC063xCC002","CC051xCC009","CC034xCC016",
-#            "CC016xCC038","CC016xCC061","CC060xCC006")
-
-# rix.list=("CC074xCC062")
-
-
-rix.list=c("CC003xCC062")
-
-rix.subset=Map.dat[which(Map.dat$RIX %in% rix.list),]
-
-g=ggplot(phenotype,aes(x=TotalG,y=IgM))
-g+geom_point(size=4,aes(color=dam.mx1))+
-  scale_color_manual(values=cbPalette)+
-  geom_point(aes(color=sire.mx1))+
-  theme_classic()+geom_text(aes(label=RIX),size=3,vjust=-1)+
-  theme(legend.title=element_blank())+
-  geom_point(data=rix.subset,aes(x=TotalG,y=IgM),colour="red", size=5)
-
-
-# ggsave("qtls/d45/totalg_v_igm_d7_highlighted.jpg",width=10,height=6)
+          # #### two-phenotype distributions ####
+          # library(doBy)
+          # dat=dat.10
+          # 
+          # Map.dat=summaryBy(IgG1+IgG2ac+IgG2b+IgG3+IgM+TotalG ~ 
+          #                     RIX + day, data=dat, FUN=mean, na.rm=T)
+          # colnames(Map.dat)[3:8] = gsub(".mean","",colnames(Map.dat[3:8]))
+          # Map.dat$RIX=as.character(Map.dat$RIX)
+          # 
+          # #code with Mx1 status
+          # 
+          # RIX_sep <- data.frame(do.call("rbind", strsplit(Map.dat$RIX,"x")))
+          # colnames(RIX_sep)[1:2]=c("dam","sire")
+          # 
+          # Map.dat=cbind(RIX_sep,Map.dat)
+          # 
+          # mx1=read.csv("~/Dropbox/Heise/CC/mx1_status.csv")
+          # mx1=mx1[c(1,8)]
+          # 
+          # colnames(mx1)[1]="dam"
+          # Map.dat=merge(Map.dat,mx1)
+          # colnames(Map.dat)[11]="dam.mx1"
+          # 
+          # colnames(mx1)[1]="sire"
+          # Map.dat=merge(Map.dat,mx1)
+          # colnames(Map.dat)[12]="sire.mx1"
+          # 
+          # Map.dat=Map.dat[c(1,12,2,11,3:10)]
+          # 
+          # #graphs
+          # Map.dat$RIX=as.factor(Map.dat$RIX)
+          # 
+          # phenotype=Map.dat[c(1:6,12,11)]
+          # phenotype=phenotype[complete.cases(phenotype),]  
+          # 
+          # cbPalette <- c("royalblue","orchid","seagreen2")
+          # 
+          # g=ggplot(phenotype,aes(x=TotalG,y=IgM))
+          # g+geom_point(size=4,aes(color=dam.mx1))+
+          #   scale_color_manual(values=cbPalette)+
+          #   geom_point(aes(color=sire.mx1))+
+          #   theme_classic()+geom_text(aes(label=RIX),size=3,vjust=-1)+
+          #   theme(legend.title=element_blank())
+          # 
+          # 
+          # # rix.list=c("CC037xCC046","CC063xCC002","CC051xCC009","CC034xCC016",
+          # #            "CC016xCC038","CC016xCC061","CC060xCC006")
+          # 
+          # # rix.list=("CC074xCC062")
+          # 
+          # 
+          # rix.list=c("CC003xCC062")
+          # 
+          # rix.subset=Map.dat[which(Map.dat$RIX %in% rix.list),]
+          # 
+          # g=ggplot(phenotype,aes(x=TotalG,y=IgM))
+          # g+geom_point(size=4,aes(color=dam.mx1))+
+          #   scale_color_manual(values=cbPalette)+
+          #   geom_point(aes(color=sire.mx1))+
+          #   theme_classic()+geom_text(aes(label=RIX),size=3,vjust=-1)+
+          #   theme(legend.title=element_blank())+
+          #   geom_point(data=rix.subset,aes(x=TotalG,y=IgM),colour="red", size=5)
+          # 
+          # 
+          # # ggsave("qtls/d45/totalg_v_igm_d7_highlighted.jpg",width=10,height=6)
 
 
 #### mapping ####
 #load in required libraries and data
 library(doBy)
 library(DOQTL)
-load("~/Dropbox/Heise/ELISA Antibody/R codes and data/CCRIXb38F.Rdata")
+load("~/Dropbox/Heise/U19-Ab/cc_refs/CCRIXb38F.Rdata")
 # load(url("ftp://ftp.jax.org/MUGA/MM_snps.Rdata"))
-load("~/Dropbox/Heise/ELISA Antibody/R codes and data/MM_snps.Rdata")
+load("~/Dropbox/Heise/U19-Ab/cc_refs/MM_snps.Rdata")
 
 model.probs<-model.probs+(1e-20)
 K=kinship.probs(model.probs)
 
 ## to graph QTL scans (no significance threshold yet)
 
-for(k in c(1,4))
+for(k in c(2))
   # for(k in 1:4)
 {
   day=day.list[[k]]
@@ -212,24 +178,25 @@ for(k in c(1,4))
   
   Map.dat = summaryBy(IgG1+IgG2ac+IgG2b+IgG3+IgM+TotalG ~ 
                         RIX + day, data=dat, FUN=mean, na.rm=T)
-  colnames(Map.dat)[3:8] = gsub(".mean","",colnames(Map.dat[3:8]))
+      colnames(Map.dat)[3:8] = gsub(".mean","",colnames(Map.dat[3:8]))
+      
+      Map.dat$Sex="F"
+      Map.dat$RIX=as.factor(Map.dat$RIX)
+      
+      row.names(Map.dat)<-Map.dat$RIX
+      covar = data.frame(sex=as.numeric(Map.dat$Sex == "F"))
+      rownames(covar)=rownames(Map.dat)
   
-  Map.dat$Sex="F"
-  Map.dat$RIX=as.factor(Map.dat$RIX)
-  
-  row.names(Map.dat)<-Map.dat$RIX
-  covar = data.frame(sex=as.numeric(Map.dat$Sex == "F"))
-  rownames(covar)=rownames(Map.dat)
-  
-  png(file.path(paste("qtls/d",day,sep=""),paste("qtl_scan_d",day.list[[k]],"_",run.date,".png",sep="")),width=1000,height=500)
+  png(file.path(paste("phenotypes/d",day,sep=""),paste("qtl_scan_d",day,"_",data.type,".png",sep=""))
+    ,width=1000,height=500)
   par(mfrow = c(2,3))
   for(i in 1:6)
   {
     pheno=abs[i]
     qtl=scanone(pheno=Map.dat, pheno.col=pheno, addcovar=covar, probs=model.probs, K=K, snps=MM_snps)
     plot(qtl,main=paste("Day",day.list[[k]],pheno,sep=" "))
-    saveRDS(qtl,file.path(paste("qtls/d",day,sep=""),paste("qtl_scan_d",day,"_",abs[i],"_",run.date,".Rdata",sep="")))
-    #     save(qtl,file=file.path(paste("qtls/d",day,sep=""),paste("qtl_scan_d",day,"_",abs[i],".RData",sep="")))
+    save(qtl,file=file.path(paste("phenotypes/d",day,sep=""),paste("d",day,"_",abs[i],"_",
+                                                                   data.type,"_qtl_obj",".RData",sep="")))
   }
   dev.off()
 }
@@ -237,39 +204,45 @@ for(k in c(1,4))
 # save PDFs w/ width of 11 and height of 6
 # save PNGs w/ width of 1000 and height of 500
 
-#### to graph with thresholds (takes a long time!) ####
-#using a threshold of 0.9 for suggestive QTL
+#### permutation test to calculate threshold ####
+#set number of permutations to run and threshold
+nperm=100
+probs=.95
 
-for(k in c(1,4))
-  # for(k in 1:4)
+for(k in c(2))
+# for(k in 1:4)
 {
   day=day.list[[k]]
   dat=dat.list[[k]]
   
   Map.dat = summaryBy(IgG1+IgG2ac+IgG2b+IgG3+IgM+TotalG ~ 
                         RIX + day, data=dat, FUN=mean, na.rm=T)
-  colnames(Map.dat)[3:8] = gsub(".mean","",colnames(Map.dat[3:8]))
+      colnames(Map.dat)[3:8] = gsub(".mean","",colnames(Map.dat[3:8]))
+      
+      Map.dat$Sex="F"
+      Map.dat$RIX=as.factor(Map.dat$RIX)
+      
+      row.names(Map.dat)<-Map.dat$RIX
+      covar = data.frame(sex=as.numeric(Map.dat$Sex == "F"))
+      rownames(covar)=rownames(Map.dat)
   
-  Map.dat$Sex="F"
-  Map.dat$RIX=as.factor(Map.dat$RIX)
-  
-  row.names(Map.dat)<-Map.dat$RIX
-  covar = data.frame(sex=as.numeric(Map.dat$Sex == "F"))
-  rownames(covar)=rownames(Map.dat)
-  
-  png(file.path(paste("qtls/d",day,sep=""),paste("qtl_scan_95_100_d",day.list[[k]],"_",run.date,".png",sep="")),width=1000,height=500)
-  par(mfrow = c(2,3))
   for(i in 1:6)
   {
     pheno=abs[i]
-    qtl=scanone(pheno=Map.dat, pheno.col=pheno, addcovar=covar, probs=model.probs, K=K, snps=MM_snps)
+  
+  
+    load(file.path(paste("phenotypes/d",day,sep=""),paste("d",day,"_",abs[i],"_",
+                                                          data.type,"_qtl_obj",".RData",sep="")))
     
-    perms = scanone.perm(pheno = Map.dat, pheno.col = abs[i], probs = model.probs, addcovar = covar, snps= MM_snps, nperm = 100)
-    save(perms,file=file.path(paste("qtls/d",day,sep=""),paste("perms500_d",day.list[[k]],"_",abs[i],"_",run.date,".Rdata",sep="")))
+    perms = scanone.perm(pheno = Map.dat, pheno.col = abs[i], probs = model.probs, addcovar = covar, snps= MM_snps, nperm = nperm)
     
-    thr = quantile(perms, probs = 0.90)    
+    save(perms,
+         file=file.path(paste("phenotypes/d",day,sep=""),
+                   paste("d",day,"_",abs[i],"_",data.type,"_perms_",nperm,".Rdata",sep="")))
     
-    plot(qtl,sig.thr=thr,main=paste("Day",day.list[[k]],pheno,sep=" "))
+    thr = quantile(perms, probs = probs)    
+    
+    plot(qtl,sig.thr=thr,main=paste("Day",day,pheno,data.type,sep=" "))
     
     
   }
@@ -277,90 +250,27 @@ for(k in c(1,4))
 }
 
 
-
-
-
-
-
-
-
-
-
-
-#### zoom in on chromosomes with significant/suggestive QTL to look at range and allele effects ####5
-pheno="IgG"
-qtl=scanone(pheno=Map.dat, pheno.col=pheno, addcovar=covar, probs=model.probs, K=K, snps=MM_snps)
-
-chr=5
-
-plot(qtl, main=paste("Day",day,pheno,sep=" "))
-coefplot(qtl, chr=chr,legend=F)
-coefplot_v2(qtl, chr=chr,main=paste("Day",day,pheno,"Chromosome",chr,sep=" "),legend=F)
-
-#### mapping with saved qtl objects and permutations ####
-# for(k in 1:4)
-#{
-
-date.to.load = "2017_01_16"
-
-k=2
-
-day=day.list[[k]]
-dat=dat.list[[k]]
-
-Map.dat = summaryBy(IgG1+IgG2ac+IgG2b+IgG3+IgM+TotalG ~ 
-                      RIX + day, data=dat, FUN=mean, na.rm=T)
-colnames(Map.dat)[3:8] = gsub(".mean","",colnames(Map.dat[3:8]))
-
-Map.dat$Sex="F"
-Map.dat$RIX=as.factor(Map.dat$RIX)
-
-row.names(Map.dat)<-Map.dat$RIX
-covar = data.frame(sex=as.numeric(Map.dat$Sex == "F"))
-rownames(covar)=rownames(Map.dat)
-
-png(file.path(paste("qtls/d",day,sep=""),paste("qtl_scan_95_d",day.list[[k]],"_",run.date,".png",sep="")),width=1000,height=500)
-par(mfrow = c(2,3))
-for(i in 1:6)
-{
-  pheno=abs[i]
-  load(file=file.path(paste("qtls/d",day,sep=""),paste("qtl_scan_d",day.list[[k]],"_",abs[i],".Rdata",sep="")))
-  
-  #perms = 
-  load(file=file.path(paste("qtls/d",day,sep=""),paste("perms500_d",day.list[[k]],"_",abs[i],"_",date.to.load,".Rdata",sep="")))
-  
-  thr = quantile(perms, probs = 0.95)    
-  
-  plot(qtl,sig.thr=thr,main=paste("Day",day.list[[k]],pheno,sep=" "))
-}
-dev.off()
-}
-
-
 ### zoom in on chromosomes with significant/suggestive QTL to look at range and allele effects ###
-day=7
-pheno="IgG2ac"
-load(file=file.path(paste("qtls/d",day,sep=""),paste("qtl_scan_d",day,"_",pheno,"_",date.to.load,".Rdata",sep="")))
+day=10
+pheno="IgG3"
+load(file=file.path(paste("phenotypes/d",day,sep=""),paste("d",day,"_",pheno,"_",
+                                                      data.type,"_qtl_obj",".RData",sep="")))
 
-chr=3
+chr=11
 
 # plot(qtl, main=paste("Day",day,pheno,sep=" "))
-# coefplot(qtl, chr=chr,legend=F)
-coefplot_v2(qtl, chr=chr,main=paste("Day",day,pheno,"Chromosome",chr,sep=" "),legend=F)
-
-# plot(lod[,3], coef[,colors[1,1]], type = "l", col = colors[1,3], lwd = 2,
-#      ylim = c(-3, 3), xlab = 
-#        paste("Chr", chr, "(Mb)"), ylab = "Founder Effects", axes = FALSE, ...)
-
-# str(qtl$lod$A)
+coefplot(qtl, chr=chr,legend=F)
+# coefplot_v2(qtl, chr=chr,main=paste("Day",day,pheno,"Chromosome",chr,sep=" "),legend=F)
 
 ### to get regions of genome that fall under significance threshold
-load(file=file.path(paste("qtls/d",day,sep=""),paste("perms100_d",day.list[[k]],"_",pheno,"_",run.date,".Rdata",sep="")))
+load(file=file.path(paste("phenotypes/d",day,sep=""),
+               paste("d",day,"_",pheno,"_",data.type,"_perms_",nperm,".Rdata",sep="")))
+
 thr = quantile(perms, probs = 0.95) 
 
-#max(qtl$lod$A$lod)
+#get marker with maximum LOD score and look at surrounding interval
 qtl$lod$A[which.max(qtl$lod$A$lod),]
-qtl$lod$A[(which.max(qtl$lod$A$lod)-50):(which.max(qtl$lod$A$lod)+20),]
+# qtl$lod$A[(which.max(qtl$lod$A$lod)-10):(which.max(qtl$lod$A$lod)+10),]
 
 #get confidence interval with 1.5 LOD drop
 lod.peak=qtl$lod$A[which.max(qtl$lod$A$lod),"lod"]
@@ -372,21 +282,30 @@ sig.range[nrow(sig.range),]
 #compare to bayesian credible interval
 bayesint(qtl,chr=11,prob=0.95,expandtomarkers=T)
 
-#get haplotype probs for RIXs at the peaks - manually 
-igg3.11.probs<-model.probs[,,qtl$lod$A[which.max(qtl$lod$A$lod),"marker"]]
-write.csv(igg3.11.probs,"qtls/d10/igg3.11.qtl.probs.csv")
+
+
+
+
+
+#get haplotype probs for RIXs at the peak - manually 
+probs<-model.probs[,,qtl$lod$A[which.max(qtl$lod$A$lod),"marker"]]
+ # write.csv(probs,file.path(paste("phenotypes/d",day,sep=""),
+ #               paste("d",day,"_",pheno,"_",data.type,"_chr",chr,"_probs",".csv",sep="")))
 
 
 #to look at a peak other than the very max, input chromosome of interest
 qtl.chr=subset(qtl$lod$A,qtl$lod$A$chr==5)
 qtl.chr[which.max(qtl.chr$lod),]
-qtl.chr[(which.max(qtl.chr$lod)-20):(which.max(qtl.chr$lod)+20),]
+# qtl.chr[(which.max(qtl.chr$lod)-10):(which.max(qtl.chr$lod)+10),]
 
 #to zoom in on qtl plot in region of interest
 qtl.chr.sub=subset(qtl.chr,qtl.chr$pos>63 & qtl.chr$pos<75)
 # plot(x=qtl.chr.sub$pos,y=qtl.chr.sub$lod)
 ggplot(qtl.chr.sub, aes(pos, lod))+geom_point()+theme_minimal()
 
+
+
+#####where i am
 
 
 ## to graph QTL scans w/ Mx1 status as covariate
@@ -423,7 +342,7 @@ covar = data.frame(sex=as.numeric(Map.dat$Sex == "F"),mx1=Map.dat$mx1.sum)
 rownames(covar)=rownames(Map.dat)
 
 
-for(i in 1:6)
+# for(i in 1:6)
 {
   fp=paste("qtls/qtl_scan_mx1_covar_d",day.list[k],"_",pheno,"_",run.date,sep="")
   pheno=abs[i]
